79fa074b84c0dd7b44c32b369179e361cc4d31b0
Davide Giambona <86895372+davidegiambona@users.noreply.github.com>
2023-04-27 09:08:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-fetch("https://raw.githubusercontent.com/davidegiambona/prova/main/index.json")
-.then(res => console.log(res.json()))
+fetch("https://raw.githubusercontent.com/davidegiambona/fetch-and-then-json-example/main/index.json")
+.then(response => console.log(response.json()))
+.then(json => parseText(json))
 // Create an object:
+
+function parseText(myJson){
+    let fullResponse = myJson; 
+    console.log(fullResponse);
+
+    let keys = Object.keys(fullResponse); // estrae le chiavi del dizionario
+    for (let k = 0; k < keys.length; k++){ // per ogni chiave
+      let currentKey = keys[k]; // estrare l'elemento corrente      
+      if (currentKey == "div"){ // se l'elemento corrente è un div
+         let div = document.createElement("div"); // crea un div
+         div.id = fullResponse[currentKey]["id"]; // assegna l'id
+         div.innerHTML = fullResponse[currentKey]["innerHTML"]; // assegna il contenuto del div
+         document.body.appendChild(div); // visualizza l'elemento in fondo alla pagina html
+      }      
+    }
+
+/*
 const person = {
   firstName: "John",
   lastName: "Doe",
@@ -12,3 +30,4 @@ const person = {
 
 // Display data from the object:
 document.getElementById("demo").innerHTML = person.fullName();
+*/
